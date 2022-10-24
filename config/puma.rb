7ea2fd5c002e8c56681efa6643a432cabe2e0dc6
suilen-ren <we.are.crazy.bassist@gmail.com>
2022-10-24 07:17:11 +0000

    puma.rbの設定を編集

--- config/puma.rb
+++ config/puma.rb
@@ -46,7 +46,7 @@ rails_root = Dir.pwd
   if Rails.env.production?
     pidfile File.join(rails_root, 'tmp', 'pids','puma.pid')
     state_path File.join(rails_root, 'tmp','pids','puma.state')
-    stdout_reidrect(
+    stdout_redirect(
       File.join(rails_root,'log','puma.log'),
       File.join(rails_root,'log','puma-error.log'),
       true
