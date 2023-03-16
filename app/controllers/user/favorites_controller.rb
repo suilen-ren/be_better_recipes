991f89509e03d13e3a42b5f71fbec1c85df4963a
suilen-ren <we.are.crazy.bassist@gmail.com>
2023-03-16 06:53:39 +0000

    wip - favoriteの非同期化

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -1,19 +1,19 @@
 class User::FavoritesController < ApplicationController
   before_action :authenticate_user!
   before_action :reject_guest_user
-  
+
   def create
     recipe = Recipe.find(params[:recipe_id])
     favorite = current_user.favorites.new(recipe_id: recipe.id)
     favorite.save
-    redirect_to recipe_path(recipe),notice: "レシピをお気に入りに追加しました"
+    # redirect_to recipe_path(recipe),notice: "レシピをお気に入りに追加しました"
   end
 
   def destroy
     recipe = Recipe.find(params[:recipe_id])
     favorite = current_user.favorites.find_by(recipe_id: recipe.id)
     favorite.destroy
-    redirect_to recipe_path(recipe),notice: "レシピをお気に入りから削除しました"
+    # redirect_to recipe_path(recipe),notice: "レシピをお気に入りから削除しました"
   end
 
 
